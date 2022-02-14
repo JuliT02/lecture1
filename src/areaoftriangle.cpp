c4471fb5fdf78daa71d689cd9b0ef37acaabd9cc
JuliT02 <julian.toro1@upr.edu>
2022-02-14 17:56:35 -0500

    Removed uneccesary getArea function and some libraries.

--- src/areaoftriangle.cpp
+++ src/areaoftriangle.cpp
@@ -1,18 +1,7 @@
-#include <cmath>
 #include <iostream>
-#include <string>
-#include <cctype>
 #include "include/Triangle.h"
 
 using namespace std;
-
-double computeTriangleArea(Triangle t){
-    //Esta funcion calcula la area de un triangulo con el input de los 3 lados.
-    //Este es el body de la funcion, aqui se coloca el agoritmo que se uso en areaofatriangle.cpp 
-    double s = (t.getSide1() + t.getSide2() + t.getSide3()) / 2;
-    double area = sqrt(s * (s - t.getSide1()) * (s - t.getSide2()) * (s - t.getSide3()));
-    return area;
-}
 
 int main(){
     //Calcula 3 areas diferentes con la funcion que ya definimos.
